edde46d92644020a9d4abc455f752ff6c9de09d9
sithara <sithara.chakkath@gmail.com>
2015-07-26 14:46:21 -0700

    todays changes in trnsportation

--- trasnportation/Transportation.java
+++ trasnportation/Transportation.java
@@ -5,6 +5,7 @@ public abstract class Transportation {
 	/* to get total # of trasnporations under the specified category */
 	public int getTotalTransporations() {
 		//query the database to get all the Transportations 
+		//sithara changes go here
 		return 1;
 	}
 	
